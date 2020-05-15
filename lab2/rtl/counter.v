5fd60507d9f48a9c59f31cdca56030d70c57bd0c
Григорьева Анастасия <51454241+SourMandarin@users.noreply.github.com>
2020-05-15 21:21:06 +0300

    Правки

--- lab2/rtl/counter.v
+++ lab2/rtl/counter.v
@@ -30,19 +30,20 @@ module counter
   output [6:0] hex2_o
   );
 
-reg              b_event     = 1'd1;
+reg              b_event;
+reg              b_event2;
 reg      [9:0]   p;
 reg      [7:0]   counter;
 wire     [2:0]   button_down;
 wire     [11:0]  counter_dec;
 
-button_check bt0
-  (
-  .reset  ( key_i[1]       ),
-  .btn_i  ( !key_i[0]      ),
-  .ondn_o ( button_down[0] ),
-  .clk_i  ( clk100_i       )
-  );
+//button_check bt0
+//  (
+//  .reset  ( key_i[1]       ),
+//  .btn_i  ( !key_i[0]      ),
+//  .ondn_o ( button_down[0] ),
+//  .clk_i  ( clk100_i       )
+//  );
 
 //button_check bt1
  // (
@@ -51,31 +52,39 @@ button_check bt0
  // .clk_i  ( clk100_i       )
  // );
 
-button_check bt2
-  (
-  .reset  ( key_i[1]       ),
-  .btn_i  ( !key_i[2]      ),
-  .ondn_o ( button_down[2] ),
-  .clk_i  ( clk100_i       )
-  );
+//button_check bt2
+//  (
+//  .reset  ( key_i[1]       ),
+//  .btn_i  ( !key_i[2]      ),
+//  .ondn_o ( button_down[2] ),
+//  .clk_i  ( clk100_i       )
+//  );
 
 always @( posedge clk100_i or negedge key_i[1] ) begin
   if ( !key_i[1] ) begin
-    p           <= 10'b0;
-    counter     <= 8'd0;
-    b_event     <= 1'd1;
+    p            <= 10'b0;
+    counter      <= 8'd0;
+    b_event      <= 1'd0;
+    b_event2     <= 1'd0;
   end
   else begin
-    if ( !key_i[0] && !button_down[0] && b_event ) begin
-      if ( button_down[2] ) begin
-        counter <= counter + 1'b1;
-        p       <= sw_i;
-        b_event <= 1'd0;
+    if ( !key_i[0] && key_i[2] && !b_event2 ) begin
+      b_event <= 1'd1;
+    end
+    else begin
+      if ( key_i[0] )
+        b_event2 <= 1'd0;
+      if ( b_event ) begin
+        if ( !key_i[2] && !key_i[0] ) begin
+          counter  <= counter + 1'b1;
+          p        <= sw_i;
+          b_event  <= 1'd0;
+          b_event2 <= 1'd1;
+        end
+        else
+          b_event <= 1'd0;
       end
     end
-    else
-      if ( button_down[0] && !key_i[0] )
-        b_event <= 1'd1;
   end
 end
 
